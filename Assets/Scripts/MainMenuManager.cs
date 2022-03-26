265c7a69c0a2b67fcce1bed62c508817d36c7a2f
Jeferson Tomazella <jefbt@outlook.com>
2022-03-26 09:45:34 -0300

    Menu Screen Added
    - Start Game Button
    - Exit Button

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
